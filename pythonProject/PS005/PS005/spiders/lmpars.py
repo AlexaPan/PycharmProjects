febd2a1493cc0d8e9dd76a5c86b1a8b778a6b239
AlexaPan <xvspan@gmail.com>
2025-02-26 21:58:08 +0300

    ps05

--- pythonProject/PS005/PS005/spiders/lmpars.py
+++ pythonProject/PS005/PS005/spiders/lmpars.py
@@ -7,4 +7,15 @@ class LmparsSpider(scrapy.Spider):
     start_urls = ["https://lm-catalog.ru/product-category/osveshhenie/osveshhenie-zhilyh-pomeshhenij/torshery/"]
 
     def parse(self, response):
-        pass
+        torshers = response.css('div.shop-item-inner')
+        for torsher in torshers:
+            yield {
+                'name': torsher.css('div.shop-item__title a::text').get(),
+                'price': torsher.css('div.shop-item__price bdi::text').get(),
+                'link': torsher.css('div.shop-item__title a::attr(href)').get()
+            }
+
+        # Опционально: обработка пагинации
+        next_page = response.css('a.next.page-numbers::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
